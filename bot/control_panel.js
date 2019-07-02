172b661f0cd86792d3381f35b35af2ab6f7d5692
Danil Lyashenko <shub21680@gmail.com>
2019-07-02 19:54:14 +0300

    refactor control_panel P1

--- bot/control_panel.js
+++ bot/control_panel.js
@@ -1,16 +1,17 @@
 const DB = require("./database");
 //spam to
+const queryPrefix = "CP: ";
 const openKeyboard = [
   [
     {
       text: "get user id",
-      callback_data: "CP: " + "get chat id"
+      callback_data: "CP: " + "getChatId"
     }
   ],
   [
     {
       text: "kick user",
-      callback_data: "CP: " + "kick user"
+      callback_data: "CP: " + "kickUser"
     }
   ],
   [
@@ -28,7 +29,7 @@ const openKeyboard = [
   [
     {
       text: "close panel",
-      callback_data: "CP: " + "close panel"
+      callback_data: "CP: " + "closePanel"
     }
   ]
 ];
@@ -38,16 +39,11 @@ let msgChatsKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
 let usersKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
 let groupKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
 
-let users = DB.data.userMessageCount;
+let userMessageCount = DB.data.userMessageCount;
 
 function createKeyboard(chats, keyboard, callback_key) {
   chats.forEach(chat => {
-    let name = chat.first_name + " " + chat.last_name;
-    let type = chat.type;
-    if (type === "group" || type === "supergroup") {
-      name = chat.title;
-    }
-    keyboard.unshift([{ text: name, callback_data: callback_key + chat.id }]);
+    createCurrentKeyboard(chat, keyboard, callback_key);
   });
 }
 
@@ -70,123 +66,124 @@ function editMessage(newText, chatId, msgId, keyboard) {
   });
 }
 
-let msgId; //for edit panel
 let idToSend; // for sending message (chat_id)
 
 bot.onText(/\/controlP/, function(msg) {
   const chatId = msg.chat.id;
   if (chatId === 462509174) {
-    if (msgId != null) {
-      editMessage("Panel has been closed", chatId, msgId, []);
-      msgId = null;
-    }
-    bot
-      .sendMessage(chatId, "Control_panel v.1.0 :", {
-        reply_markup: {
-          inline_keyboard: openKeyboard
-        }
-      })
-      .then(ms => (msgId = ms.message_id));
+    bot.sendMessage(chatId, "Control_panel v.1.0 :", {
+      reply_markup: {
+        inline_keyboard: openKeyboard
+      }
+    });
   }
 });
 
 bot.on("callback_query", query => {
-  if (query.data.startsWith("CP: ")) {
-    const id = query.message.chat.id;
-    if (query.data === "CP: " + "get chat id") {
-      chatsKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
-      Promise.all(Object.keys(users).map(key => bot.getChat(key)))
-        .then(chats => {
-          createKeyboard(chats, chatsKeyboard, "CP: chat ");
-        })
-        .then(result => {
-          editMessage("Control_panel v.1.0 :", id, msgId, chatsKeyboard);
-        });
-    } else if (query.data.startsWith("CP: chat ")) {
-      bot.sendMessage(id, query.data.split(" ")[2]);
-    } else if (query.data === "CP: " + "sendMessage") {
-      msgChatsKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
-      Promise.all(Object.keys(users).map(key => bot.getChat(key)))
-        .then(chats => {
-          createKeyboard(chats, msgChatsKeyboard, "CP: msg ");
-        })
-        .then(result => {
-          editMessage("Control_panel v.1.0 :", id, msgId, msgChatsKeyboard);
-        });
-    } else if (query.data.startsWith("CP: msg ")) {
-      idToSend = query.data.split(" ")[2];
-      editMessage(
-        "sending to: " +
-          msgChatsKeyboard.find(
-            item => item[0].callback_data === "CP: msg " + idToSend
-          )[0].text,
-        id,
-        msgId,
-        backPanel
-      );
-    } else if (query.data === "CP: " + "kick user") {
-      groupKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
-      usersKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
-      Promise.all(Object.keys(users).map(key => bot.getChat(key)))
-        .then(chats => {
-          chats.forEach(chat => {
-            if (chat.type === "supergroup" || chat.type === "group") {
-              createCurrentKeyboard(chat, groupKeyboard, "CP: group ");
-            } else {
-              createCurrentKeyboard(chat, usersKeyboard, "CP: kick ");
-            }
-          });
-        })
-        .then(result => {
-          let userId = [];
-          let groupId = [];
-          for (let i = 0; i < usersKeyboard.length - 1; i++) {
-            userId.push(usersKeyboard[i][0].callback_data.split(" ")[2]);
-          }
-          for (let g = 0; g < groupKeyboard.length - 1; g++) {
-            groupId.push(groupKeyboard[g][0].callback_data.split(" ")[2]);
-          }
+  let msgId = query.message.message_id;
 
-          usersKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
-          let usersInGroup = [];
-          new Promise((resolve, reject) => {
-            userId.forEach(key => {
-              for (let g = 0; g < groupId.length; g++) {
-                bot.getChatMember(groupId[g], key).then(data => {
-                  usersInGroup.push(data);
-                  if (data.status === "member") {
-                    let name = data.user.first_name + " " + data.user.last_name;
-                    let title = groupKeyboard.find(
-                      item =>
-                        item[0].callback_data === "CP: group " + groupId[g]
-                    )[0].text;
-                    usersKeyboard.unshift([
-                      {
-                        text: name + " " + title,
-                        callback_data:
-                          "CP: kick " + data.user.id + " " + groupId[g]
-                      }
-                    ]);
+  if (!query.data.startsWith(queryPrefix)) {
+    return;
+  }
+
+  const cmd = query.data.substring(queryPrefix.length);
+  const parts = cmd.split(" ");
+  const cmdName = parts[0];
+  const args = parts.slice(0);
+
+  const id = query.message.chat.id;
+  if (cmdName === "getChatId") {
+    chatsKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
+    Promise.all(Object.keys(userMessageCount).map(key => bot.getChat(key)))
+      .then(chats => {
+        createKeyboard(chats, chatsKeyboard, "CP: chat ");
+      })
+      .then(result => {
+        editMessage("Control_panel v.1.0 :", id, msgId, chatsKeyboard);
+      });
+  } else if (cmdName === "chat") {
+    bot.sendMessage(id, args[0]);
+  } else if (cmdName === "sendMessage") {
+    msgChatsKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
+    Promise.all(Object.keys(userMessageCount).map(key => bot.getChat(key)))
+      .then(chats => {
+        createKeyboard(chats, msgChatsKeyboard, "CP: msg ");
+      })
+      .then(result => {
+        editMessage("Control_panel v.1.0 :", id, msgId, msgChatsKeyboard);
+      });
+  } else if (cmdName === "msg") {
+    console.log(
+      msgChatsKeyboard.find(
+        item => item[0].callback_data === "CP: msg " + idToSend
+      )[0].text
+    );
+    idToSend = args[0];
+    editMessage(
+      "sending to: " +
+        msgChatsKeyboard.find(
+          item => item[0].callback_data === "CP: msg " + idToSend
+        )[0].text,
+      id,
+      msgId,
+      backPanel
+    );
+  } else if (cmdName === "kickUser") {
+    groupKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
+    usersKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
+    Promise.all(Object.keys(userMessageCount).map(key => bot.getChat(key)))
+      .then(chats => {
+        chats.forEach(chat => {
+          if (chat.type === "supergroup" || chat.type === "group") {
+            createCurrentKeyboard(chat, groupKeyboard, "CP: group ");
+          } else {
+            createCurrentKeyboard(chat, usersKeyboard, "CP: kick ");
+          }
+        });
+      })
+      .then(result => {
+        let userId = [];
+        let groupId = [];
+        for (let i = 0; i < usersKeyboard.length - 1; i++) {
+          userId.push(usersKeyboard[i][0].callback_data.split(" ")[2]);
+        }
+        for (let g = 0; g < groupKeyboard.length - 1; g++) {
+          groupId.push(groupKeyboard[g][0].callback_data.split(" ")[2]);
+        }
+
+        usersKeyboard = [[{ text: "<=", callback_data: "CP: " + "<=" }]];
+        let usersInGroup = [];
+        userId.forEach(key => {
+          for (let g = 0; g < groupId.length; g++) {
+            bot.getChatMember(groupId[g], key).then(data => {
+              usersInGroup.push(data);
+              if (data.status === "member") {
+                let name = data.user.first_name + " " + data.user.last_name;
+                let title = groupKeyboard.find(
+                  item => item[0].callback_data === "CP: group " + groupId[g]
+                )[0].text;
+                usersKeyboard.unshift([
+                  {
+                    text: name + " " + title,
+                    callback_data: "CP: kick " + data.user.id + " " + groupId[g]
                   }
-                  if (usersInGroup.length === groupId.length * userId.length) {
-                    resolve(usersInGroup);
-                  }
-                });
+                ]);
+              }
+              if (usersInGroup.length === groupId.length * userId.length) {
+                editMessage("Control_panel v.1.0 :", id, msgId, usersKeyboard);
               }
             });
-          }).then(result => {
-            editMessage("Control_panel v.1.0 :", id, msgId, usersKeyboard);
-          });
+          }
         });
-    } else if (query.data.startsWith("CP: kick ")) {
-      bot.kickChatMember(query.data.split(" ")[3], query.data.split(" ")[2]);
-    } else if (query.data === "CP: " + "<=") {
-      editMessage("Control_panel v.1.0 :", id, msgId, openKeyboard);
-      idToSend = null;
-    } else if (query.data === "CP: " + "close panel") {
-      editMessage("Panel has been closed", id, msgId, []);
-      msgId = null;
-    }
+      });
+  } else if (cmdName === "kick") {
+    bot.kickChatMember(args[1], args[0]);
+  } else if (cmdName === "<=") {
+    editMessage("Control_panel v.1.0 :", id, msgId, openKeyboard);
+    idToSend = null;
+  } else if (cmdName === "closePanel") {
+    editMessage("Panel has been closed", id, msgId, []);
+    console.log("hello");
   }
 });
 
